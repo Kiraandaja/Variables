ab2fb7cd570ce1202f111c62b0eab40b93f6cc9d
Kiraandaja <103534347+Kiraandaja@users.noreply.github.com>
2022-04-23 15:35:29 -0700

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,17 @@
+﻿// See https://aka.ms/new-console-template for more information
+Console.WriteLine("Hello, World!");
+//Variable Declarations
+string name;
+int age;
+char letter;
+bool isAlive;
+double number;
+decimal secondNumber;
+//Variable Initializations
+name = "Takira";
+age = 28;
+letter = 'A';
+isAlive = true;
+number = 90.22;
+secondNumber = 19.22m;
+Console.WriteLine($"My name is {name}. This year I am {age} years old. My favorite letter is {letter}. I am alive, that is {isAlive}. My current score is {number}. It has increased by {secondNumber} points.");
